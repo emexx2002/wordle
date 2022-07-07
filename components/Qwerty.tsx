f2ba92a5e07cdca6793f0f666613a3d01f2cefd9
emexx2002 <aguchukwuemekag@gmail.com>
2022-07-07 15:13:05 +0100

    fixed

--- components/Qwerty.tsx
+++ components/Qwerty.tsx
@@ -1,29 +1,29 @@
-import { observer } from "mobx-react-lite";
+import { observer } from 'mobx-react-lite'
 
-export default observer(function QWerty({ store }) {
-  const qwerty = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+export default function Qwerty({store}) {
+  const qwerty = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
   return (
-    <div className="mt-4">
+    <div className='mt-4'>
       {qwerty.map((row) => (
         <div className="flex justify-center">
-          {row.split("").map((char) => {
+          {row.split('').map((char) => {
             const bgColor = store.exactGuesses.includes(char)
-              ? "bg-green-400"
+              ? 'bg-green-400'
               : store.inexactGuesses.includes(char)
-              ? "bg-yellow-400"
+              ? 'bg-yellow-400'
               : store.allGuesses.includes(char)
-              ? "bg-gray-400"
-              : "bg-gray-200";
+              ? 'bg-gray-400'
+              : 'bg-gray-200'
             return (
               <div
                 className={`rounded-m m-px flex h-10 w-10 items-center justify-center uppercase ${bgColor}`}
               >
                 {char}
               </div>
-            );
+            )
           })}
         </div>
       ))}
     </div>
-  );
-});
+  )
+}
